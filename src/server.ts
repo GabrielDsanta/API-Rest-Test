2678aab2464932f11e0a314f72087c897ecafb21
GabrielDSanta <eloviveiros@hotmail.com>
2023-05-12 12:50:42 -0300

    Estruturando a base do projeto, Configurando futuro banco de dados e ajustando o Knex

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import fastify from 'fastify'
+import { knex } from './database'
+
+const server = fastify()
+
+server.get('/hello', async () => {
+    const test = await knex('sqlite_schema').select('*')
+    return test
+})
+
+server.listen({
+    port: 3333
+}).then(() => {
+    console.log('server is running at 3333')
+})
